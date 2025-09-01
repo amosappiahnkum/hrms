2375567dd84da4c078ea66d3d4db301448dafc45
amosappiahnkum <amos.nkum@gmail.com>
2025-09-01 08:39:30 +0000

    update filter

--- app/Http/Controllers/InformationUpdateController.php
+++ app/Http/Controllers/InformationUpdateController.php
@@ -31,7 +31,7 @@ class InformationUpdateController extends Controller
             ], 400);
         }
 
-        $infoUpdates = InformationUpdate::query()->where('status', $request->status)->orderBy('created_at', 'desc')->paginate($request->per_page ?? 10);
+        $infoUpdates = InformationUpdate::query()->where('status', $request->status)->orderBy('created_at', 'asc')->paginate($request->per_page ?? 10);
 
         return InformationUpdateResource::collection($infoUpdates);
     }
