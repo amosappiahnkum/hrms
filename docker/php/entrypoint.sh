7a2c38d919309fa37139dfeeaa2fb764c285cdf2
amosappiahnkum <amos.nkum@gmail.com>
2025-09-18 19:31:11 +0000

    update docker

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -3,23 +3,23 @@
 set -e
 
 # Wait until MySQL is ready
-until php -r "
-try {
-    \$pdo = new PDO(
-        'mysql:host=${DB_HOST};port=${DB_PORT:-3306};dbname=${DB_DATABASE}',
-        '${DB_USERNAME}',
-        '${DB_PASSWORD}',
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-    );
-    echo \"DB ready\n\";
-    exit(0);
-} catch (Exception \$e) {
-    fwrite(STDERR, \"Waiting for DB... (\" . \$e->getMessage() . \")\n\");
-    exit(1);
-}
-"; do
-    sleep 3
-done
+#until php -r "
+#try {
+#    \$pdo = new PDO(
+#        'mysql:host=${DB_HOST};port=${DB_PORT:-3306};dbname=${DB_DATABASE}',
+#        '${DB_USERNAME}',
+#        '${DB_PASSWORD}',
+#        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+#    );
+#    echo \"DB ready\n\";
+#    exit(0);
+#} catch (Exception \$e) {
+#    fwrite(STDERR, \"Waiting for DB... (\" . \$e->getMessage() . \")\n\");
+#    exit(1);
+#}
+#"; do
+#    sleep 3
+#done
 
 #php artisan migrate --force
 
