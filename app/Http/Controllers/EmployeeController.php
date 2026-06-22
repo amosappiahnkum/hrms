06b9ad8d85a1fc0806f5f9636a1533bf703d1e6f
amosappiahnkum <amos.nkum@gmail.com>
2026-06-22 13:12:43 +0000

    fix department and position crud

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -282,7 +282,8 @@ class EmployeeController extends Controller
 
             DB::commit();
 
-            Helper::updateSRMS($request->staff_id, $employee?->contactDetail?->work_telephone);
+//            todo: make dynamic
+//            Helper::updateSRMS($request->staff_id, $employee?->contactDetail?->work_telephone);
             return new EmployeeResource($employee);
         } catch (Exception $exception) {
             Log::error('Update Employee Error', ['error' => $exception]);
